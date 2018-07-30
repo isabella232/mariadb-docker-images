772d295a67c2ea84826541f89ca7c4717a55bf03
Ian Maguire <ian.maguire@mariadb.com>
2018-07-30 18:47:33 -0400

    Single test

--- test/tests/mariadb-server-docker/run.sh
+++ test/tests/mariadb-server-docker/run.sh
@@ -3,11 +3,15 @@ set -eo pipefail
 
 image="$1"
 
-export MYSQL_ROOT_PASSWORD="r00t_passw0rd"
+export MYSQL_ROOT_PASSWORD="IamGr00t!"
 
-# verify mysql client is installed
-if ! testOutput="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" --entrypoint mysql "$image" "--version" 2>/dev/null)"; then
-	echo >&2 'Mysql not installed.'
-	exit
-fi
-[ "$testOutput" = "Mysql is installed" ]
+# # verify mysql client is installed
+# if ! testOutput="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" --entrypoint mysql "$image" "--version" 2>/dev/null)"; then
+# 	echo >&2 'Mysql not installed.'
+# 	exit
+# fi
+# [ "$testOutput" = "Mysql is installed" ]
+
+# test run	
+output="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" --entrypoint mysql "$image" "--version" 2>/dev/null)"
+[ "$output" = "Mysqld is running" ]
