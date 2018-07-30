b014adde7b73ab0ec4d6e2d75af7fb13a37d8b1f
Ian Maguire <ian.maguire@mariadb.com>
2018-07-30 16:08:21 -0400

    Fixed formatting

--- test/tests/mariadb-server-docker/run.sh
+++ test/tests/mariadb-server-docker/run.sh
@@ -5,16 +5,13 @@ image="$1"
 
 export MYSQL_ROOT_PASSWORD="IamGr00t!"
 
-# simple test which should return nothing
-export mysql_check="mysql -u root -p$MYSQL_ROOT_PASSWORD mysql -e \"select all from host;\""
-
-# test that mysql client is installed
-if ! testOutput="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" --entrypoint mysql "$image" "--version" 2>/dev/null)"; then
+# test that mysql is running
+if ! testOutput="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" --entrypoint cat "$image" "/var/run/mysqld/mysqld.pid" 2>/dev/null)"; then
 	echo >&2 'Mysqld not running.'
 	exit
 fi
 [ "$testOutput" = "Mysqld is running" ]
 
 # query test
-output="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" "$image" "mysql --version")"
+output="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" "$image" "mysql" "--version")"
 [ "$output" = "Mysql query successful" ]
