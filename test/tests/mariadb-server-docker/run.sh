e6f8c00f2f1f651d4757c0d2ec5f775f6a1ee890
Ian Maguire <ian.maguire@mariadb.com>
2018-07-30 15:33:03 -0400

    Syntax fix

--- test/tests/mariadb-server-docker/run.sh
+++ test/tests/mariadb-server-docker/run.sh
@@ -7,7 +7,6 @@ export MYSQL_ROOT_PASSWORD='IamGr00t!'
 
 # simple test which should return nothing
 export mysql_check='mysql -u root -p$MYSQL_ROOT_PASSWORD mysql -e "select * from host;"'
-echo $mysql_check
 
 # test that mysql client is installed
 if ! testOutput="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" --entrypoint mysql "$image" "--version" 2>/dev/null)"; then
