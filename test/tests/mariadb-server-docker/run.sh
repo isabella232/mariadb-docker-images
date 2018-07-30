d233b2a438117a3268882827c0bd65e3f3c33914
Ian Maguire <ian.maguire@mariadb.com>
2018-07-30 16:46:20 -0400

    Fixing false positive test

--- test/tests/mariadb-server-docker/run.sh
+++ test/tests/mariadb-server-docker/run.sh
@@ -5,13 +5,13 @@ image="$1"
 
 export MYSQL_ROOT_PASSWORD="IamGr00t!"
 
-# check mysql version
+# verify mysql install
 if ! testOutput="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" --entrypoint mysql "$image" "--version" 2>/dev/null)"; then
 	echo >&2 'Mysql not installed.'
 	exit
 fi
 [ "$testOutput" = "Mysql is installed" ]
 
-# pid check
-output="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" "$image" "cat" "/var/run/mysqld/mysqld.pid")"
+# verify mysql is running	
+output="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" "$image" cat "/var/run/mysqld/mysqld.pid")"
 [ "$output" = "Mysqld is running" ]
