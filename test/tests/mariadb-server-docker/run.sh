cc627b3fdd9de26cd9812a08a1c02cf86ff1bfb7
Ian Maguire <ian.maguire@mariadb.com>
2018-07-30 14:59:43 -0400

    Adding verbosity

--- test/tests/mariadb-server-docker/run.sh
+++ test/tests/mariadb-server-docker/run.sh
@@ -5,14 +5,11 @@ image="$1"
 
 export MYSQL_ROOT_PASSWORD='IamGr00t!'
 
-# should return a pid of 1
-export mysqld_pid="/var/run/mysqld/mysqld.pid"
-
 # simple test which should return nothing
 export mysql_check='mysql -u root -p$MYSQL_ROOT_PASSWORD mysql -e "select * from host;"'
 
 # test that mysqld is running
-if ! testOutput="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" --entrypoint cat "$image" "$mysqld_pid" 2>/dev/null)"; then
+if ! testOutput="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" --entrypoint mysql "$image" "--version" 2>/dev/null)"; then
 	echo >&2 'Mysqld not running.'
 	exit
 fi
