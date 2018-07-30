2648d40135b031a2995f3d6dbe7e05165a9c9962
Ian Maguire <ian.maguire@mariadb.com>
2018-07-30 15:19:10 -0400

    Syntax cleanup

--- test/tests/mariadb-server-docker/run.sh
+++ test/tests/mariadb-server-docker/run.sh
@@ -13,13 +13,8 @@ if ! testOutput="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD"
 	echo >&2 'Mysqld not running.'
 	exit
 fi
-echo $testOutput
 [ "$testOutput" = "Mysqld is running" ]
 
 # query test
-if ! output="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" "$image" "$mysql_check")"; then
-	echo >&2 'Mysqld query error.'
-	exit
-fi
-echo $output
+output="$(docker run --rm -e "MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD" "$image" "$mysql_check")"
 [ "$output" = "Mysql query successful" ]
